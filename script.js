399ad5e681e6ca9bbb9540069c323f85ce6d38d0
Sorokina6312 <baspketlayer@gmail.com>
2022-10-13 14:31:50 +0400

    исправлено

--- script.js
+++ script.js
@@ -5,9 +5,17 @@ function buttonClickHandler()
 
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
-    if (isNaN(value1)|| isNaN(value2)){
-        System.out.println("Error");
+
+    if (!/^[0-9]+$/.test(value1)) {
+
+        result = "В левом окне ввода должно быть число!";
     }
+    else if (!/^[0-9]+$/.test(value2)) {
+        
+        result = "В правом окне ввода должно быть число!";
+    }
+
+    else {
     var result;
     switch (selectorElem.value){
         case 'plus':
@@ -20,9 +28,16 @@ function buttonClickHandler()
             result = parseFloat(value1) * parseFloat(value2);
             break;
         case 'division':
+            if (value2 == 0)
+            {
+                result = "На ноль делить нельзя!";
+            }
+            else {
             result = parseFloat(value1) / parseFloat(value2);
             break;
+            }
     }
+}
 
     document.getElementById("results").innerText = result;
     console.log(value1,value2)
